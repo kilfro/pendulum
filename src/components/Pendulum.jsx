7c295d9b1105a1179b518c39c3562225fc7bba7b
Kirill Frolov <frolov.cyrill@gmail.com>
2021-06-18 17:32:31 +0300

    Pendulum preform

---
+++ src/components/Pendulum.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '@style/pendulum.less'
+
+const Pendulum = () => (
+    <div className="pendulum">
+        <div className="thread"/>
+    </div>
+)
+
+export default Pendulum
