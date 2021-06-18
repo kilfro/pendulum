5a95eb76c70c175eb38ff350f4b4ce3b356da25d
Kirill Frolov <frolov.cyrill@gmail.com>
2021-06-18 13:15:10 +0300

    Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,33 @@
-module.exports = {}
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'build'),
+    },
+    devServer: {
+        port: 3000,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader'],
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
+}
