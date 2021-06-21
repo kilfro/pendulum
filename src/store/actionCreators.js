d15bfb3d53685af9b9d9a37f1f80aaedc4d8fc06
Kirill Frolov <frolov.cyrill@gmail.com>
2021-06-21 11:33:38 +0300

    Add action creators

--- src/store/actionCreators.js
+++ src/store/actionCreators.js
@@ -1,3 +1,21 @@
-export const reducer = (state = {}, action) => {
-    return state;
-}
+import {SET_GRAVITY, SET_INITIAL_IMPULSE, SET_THREAD_LENGTH, SET_WEIGHT} from './actionTypes'
+
+export const setThreadLength = length => ({
+    type: SET_THREAD_LENGTH,
+    length
+})
+
+export const setWeight = weight => ({
+    type: SET_WEIGHT,
+    weight
+})
+
+export const setGravity = gravity => ({
+    type: SET_GRAVITY,
+    gravity
+})
+
+export const setInitialImpulse = impulse => ({
+    type: SET_INITIAL_IMPULSE,
+    impulse
+})
