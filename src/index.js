4a96e12bc135b089d1b5ee99ffa0ae68323e0cce
Kirill Frolov <frolov.cyrill@gmail.com>
2021-06-21 11:25:09 +0300

    Add react-redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react'
 import {render} from 'react-dom'
 import App from './App'
+import {createStore} from 'redux'
+import {reducer} from './store/actionCreators'
+import {Provider} from 'react-redux'
 
-render(<App/>, document.getElementById('root'))
+const appStore = createStore(reducer)
+
+render(
+    <Provider store={appStore}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
+)
