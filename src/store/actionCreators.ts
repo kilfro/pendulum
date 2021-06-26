a00e60cdb5fa2cb2c06eef0a7c88426031ab23bc
Kirill Frolov <frolov.cyrill@gmail.com>
2021-06-26 18:53:08 +0300

    Some refactoring

--- src/store/actionCreators.ts
+++ src/store/actionCreators.ts
@@ -1,17 +1,6 @@
 import {PendulumActionTypes} from '../types/StoreTypes'
 
-export const setLength = (length: string) => {
-    return {type: PendulumActionTypes.SET_THREAD_LENGTH, payload: length}
-}
-
-export const setWeight = (weight: string) => {
-    return {type: PendulumActionTypes.SET_WEIGHT, payload: weight}
-}
-
-export const setGravity = (gravity: string) => {
-    return {type: PendulumActionTypes.SET_GRAVITY, payload: gravity}
-}
-
-export const setImpulse = (impulse: string) => {
-    return {type: PendulumActionTypes.SET_INITIAL_IMPULSE, payload: impulse}
-}
+export const setLength = (length: string) => ({type: PendulumActionTypes.SET_THREAD_LENGTH, payload: length})
+export const setWeight = (weight: string) => ({type: PendulumActionTypes.SET_WEIGHT, payload: weight})
+export const setGravity = (gravity: string) => ({type: PendulumActionTypes.SET_GRAVITY, payload: gravity})
+export const setImpulse = (impulse: string) => ({type: PendulumActionTypes.SET_INITIAL_IMPULSE, payload: impulse})
